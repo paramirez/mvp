51982afbff0ce0edac39d824fb04b9556affb785
Pablo Ramirez <pabloandres2700@gmail.com>
2017-12-30 05:38:51 -0500

    Remueve dependencia morgan

--- app/core/app.js
+++ app/core/app.js
@@ -1,14 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import morgan from 'morgan';
 import { PORT } from './global';
 import mainRouter from '../routes/main.router';
 
 const app = express();
 
 app.use(cors());
-app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(mainRouter);
 
